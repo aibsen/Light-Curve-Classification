baf67c71f6e681463b95cc36f625e692438db3f6
Amanda Ibsen <amanda.ibsen@gmail.com>
2017-08-26 18:24:57 -0300

    calculating features for entire dataset

--- light_curves.py
+++ light_curves.py
@@ -4,17 +4,17 @@ import FATS as ft
 from sklearn.svm import SVC
 from sklearn import datasets
 
-def get_features(number_of_features, trainning_set_length):
+def get_features(trainning_set_length):
     # getting features (X)
     featureL=[
-        # 'Amplitude', #IndexError
+        'Amplitude',
         'Autocor_length',
         # 'AndersonDarling', #IndexError
         'Beyond1Std',
         # 'CAR_sigma', #runtime warning
         # 'CAR_mean', #depends on car_sigma,
         # "CAR_tau", #depends on car_sigma,
-        'Con',
+        # 'Con',
         # 'Color', #depends on mag2
         # 'Eta_e', #gives nan :/
         # 'FluxPercentileRatioMid20', #IndexError
@@ -23,37 +23,41 @@ def get_features(number_of_features, trainning_set_length):
         # 'FluxPercentileRatioMid65', #IndexError
         # 'FluxPercentileRatioMid80', #IndexError,
         # 'Freq1_harmonics_amplitude_0', #IndexError
-        'LinearTrend',
-        # 'MaxSlope', #also gives nan for some values :/
+        # 'LinearTrend',
+        'MaxSlope', #also gives nan for some values :/
         'Mean',
-        'Meanvariance',
-        'MedianAbsDev',
-        'MedianBRP',
-        'PairSlopeTrend',
-        'PercentAmplitude',
+        # 'Meanvariance',
+        # 'MedianAbsDev',
+        # 'MedianBRP',
+        # 'PairSlopeTrend',
+        # 'PercentAmplitude',
         # 'PercentDifferenceFluxPercentile', #IndexError
         # 'PeriodLS', #TypeError
         # 'Period_fit', #depends on PeriodLS
         # 'Psi_CS', #depends on PeriodLS
         # 'Psi_eta', #depends on PeriodLS,
-        'Q31',
+        # 'Q31',
         # 'Q31_color', #needs mag2
-        "Rcs",
-        "Skew",
+        # "Rcs",
+        # "Skew",
         # "SlottedA_length", #ValueError when printing results
-        "SmallKurtosis",
-        "Std",
+        # "SmallKurtosis",
+        "Std"
         # "StetsonJ", #depends on mag2, error2
-        'StetsonK'
+        # 'StetsonK',
         # 'StetsonJ', #Index Error
         # 'StetsonK_AC', #Runtime warning
         # 'StetsonL', #depends on mag2, error2
         # 'VariabilityIndex' #not found
         ]
+    # featureList = "all"
+    fs = ft.FeatureSpace(featureList=featureL)
+    # fs = ft.FeatureSpace(Data=["magnitude", "time", "error"])
+
+    number_of_features = len(featureL)
     X = np.empty((0,number_of_features), float)
     Y = np.empty((0,0), float)
 
-    fs = ft.FeatureSpace(featureList=featureL)
     filenameLC = "data/AllVar_cleaned.csv"
     filenameY = "data/classes.csv"  # Directory of data for processing
     data = pd.read_csv(filenameLC, sep=",", names=["id", "time", "mag", "error"])
@@ -61,7 +65,7 @@ def get_features(number_of_features, trainning_set_length):
     ids = data.id.unique()
     number_of_objects = len(ids)
 
-    for id in ids[:trainning_set_length]: #I'm calculating only the first 15 xi, since it's just a test and I don't want to kill my pc
+    for id in ids:#[:trainning_set_length]: #I'm calculating only the first 15 xi, since it's just a test and I don't want to kill my pc
         print "calculating features for",id
         data_i = data[data.id==id]
         preprocessed_data = ft.Preprocess_LC(data_i['mag'].tolist(), data_i['time'].tolist(), data_i['error'].tolist())
@@ -83,9 +87,9 @@ def train_SVM(X, Y, trainning_set_length):
 
 if __name__ == "__main__":
     #load data
-    number_of_features = 16
+    number_of_features = 17
     trainning_set_length = 15
-    [X,Y] = get_features(number_of_features,trainning_set_length)
+    [X,Y] = get_features(trainning_set_length)
     np.save("features", X)
     np.save("classes",Y)
     # print X.shape
