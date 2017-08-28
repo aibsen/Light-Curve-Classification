eb62543eb3795dde257287b7e6d1992a17c74482
danielk605 <eunsangk@usc.edu>
2017-08-28 06:05:36 -0700

    Choose classes for pairwise plots

--- pairwiseplots.py
+++ pairwiseplots.py
@@ -7,9 +7,20 @@ import matplotlib.pyplot as plt
 
 feat_df = pd.read_csv("Features_many_sorted.csv", index_col=0, header=0)
 feat_names = list(feat_df.columns.values)
-feat_df = feat_df[[feat for feat in feat_names if ("SlottedA" not in feat) and ("StetsonK" not in feat)]]
+feat_df = feat_df[[feat for feat in feat_names if ("SlottedA" not in feat) and ("StetsonK_AC" not in feat)]]
 feat_df = feat_df[~feat_df.isin([np.nan, np.inf, -np.inf]).any(1)]
 feat_names = list(feat_df.columns.values)
 print(feat_names)
-sns.pairplot(feat_df, hue="Var_Type", vars=feat_names[5:8], dropna=True)
+fn_cur = []
+fn_cur.extend(feat_names[:3])
+fn_cur.extend(feat_names[8:10])
+sns.pairplot(feat_df, hue="Var_Type", vars=fn_cur, dropna=True)
+plt.show()
+
+# Subset classes 1 and 5
+feat_df_sub = feat_df[feat_df["Var_Type"].isin([1, 5])]
+fn_cur = []
+fn_cur.extend(feat_names[:3])
+fn_cur.extend(feat_names[8:10])
+sns.pairplot(feat_df_sub, hue="Var_Type", vars=fn_cur, dropna=True)
 plt.show()
