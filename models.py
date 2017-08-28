2159028454f750a6461721e4942ace5775d1d231
Amanda Ibsen <amanda.ibsen@gmail.com>
2017-08-28 01:18:50 -0300

    trying out classification models

---
+++ models.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+from sklearn import preprocessing
+
+def load_data(filename):
+    #reading data from csv
+    data = pd.read_csv(filename, sep=",", names=["id", "amplitude", "beyond1std", "pairslopetrend","y"], skiprows=1)
+    X = data[["id","amplitude", "beyond1std", "pairslopetrend"]]
+    Y = data["y"] 
+    #how many classes?
+    class_list = Y.unique()
+    number_of_classes = len(class_list)
+    print "there are", number_of_classes,"classes :",class_list
+    #converting data to numpy arrays, so we can split dataset
+    Xnp = X.as_matrix()
+    #normalize X
+    # Xnp = preprocessing.normalize(Xnp,norm="l2")
+    Ynp = Y.as_matrix()
+    # splitting dataset into trainning and test data
+    print"splitting data into trainning and test data sets"
+    X_train, X_test, y_train, y_test = train_test_split(Xnp, Ynp, test_size=0.3)
+    size_of_trainning = X_train.shape
+    size_of_test = X_test.shape
+    print "size of trainning data set:",size_of_trainning[0]
+    print "size of test data set:",size_of_test[0]
+
+    #checking that sets are balanced
+    Ytrain_pd = pd.DataFrame({"id":y_train})
+    Ytest_pd = pd.DataFrame({"id":y_test})
+    for tag in class_list:
+        print "class",tag,"accounts for:"
+        part_of_trainning = float(len(Ytrain_pd[Ytrain_pd.id==tag].index))/float(len(Ytrain_pd.index))
+        part_of_test = float(len(Ytest_pd[Ytest_pd.id==tag].index))/float(len(Ytest_pd.index))
+        print("%.3f of trainning data"% part_of_trainning)
+        print("%.3f of test data"% part_of_test)
+    
+    return X_train, X_test, y_train, y_test
+
+def train_SVM(Xtrain, Ytrain, Xtest, Ytest):
+    model = SVC()
+    print("training SVM")
+    model.fit(Xtrain, Ytrain)
+    print("testing SVM")
+    predicted = model.predict(Xtest)
+    #check how prediction went
+    print(metrics.classification_report(Ytest, predicted))
+    # print(metrics.confusion_matrix(expected, predicted))
+
+def train_DecisionTree(Xtrain, Ytrain, Xtest, Ytest):
+    model = DecisionTreeClassifier()
+    print("training Desicion Tree Classifier")
+    model.fit(Xtrain, Ytrain)
+    print("testing Desicion Tree Classifier")
+    predicted = model.predict(Xtest)
+    #check how prediction went
+    print(metrics.classification_report(Ytest, predicted))
+    # print(metrics.confusion_matrix(expected, predicted))
+
+if __name__ == "__main__":
+    print("LOADING DATA")
+    filename = "data/Features.csv" 
+    Xtrain, Xtest, Ytrain, Ytest = load_data(filename)
+    # print("SVM CLASSIFIER")
+    # train_SVM(Xtrain, Ytrain, Xtest, Ytest)
+    print("DECISION TREE CLASSIFIER")
+    train_DecisionTree(Xtrain, Ytrain, Xtest, Ytest)
+    print("done")
